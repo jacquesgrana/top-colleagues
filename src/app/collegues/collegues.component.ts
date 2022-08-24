f317e0e5295cc8251e9f71a6051607ce35e9bf45
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-08-24 18:09:06 +0200

    fixes7

--- src/app/collegues/collegues.component.ts
+++ src/app/collegues/collegues.component.ts
@@ -2,27 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tc-collegues',
-  template: `
-  <h1>Ici un template</h1>
-  <!-- [] => binding de propriete -->
-  <img [src]="uneImage" alt="une image par binding">
+  template: ` <h1>Ici un template</h1>
+              <!-- [] => binding de propriété-->
+              <img [src]="uneImage" alt="une Image par binding" [ngClass]="'logo'">
 
-  <!-- directive de structure ANGULAR -->
-  <p *nbIf="unChamp">
-    <!-- {{}} : interpolation : affiche d'une variable ou
-  d'un retour de méthode -->
-
-    {{unChamp}}
-    <!-- pipe : transforme l'affichage de la données -->
-
-    {{unChamp | uppercase}}
-  </p>
-  `
+              <!-- *=> directive de structure d'angular -->
+              <p *ngIf="unChamp">
+                    <!-- {{ }} =>interpolation l'affichage d'une variable ou
+                    d'un retour d'une méthode par exemple -->
+                    {{ unChamp }}
+                    <!-- pipe => ça permet de transformer l'affichage de la donnée -->
+                    {{ unChamp | uppercase}}
+              </p>
+  `,
+  styleUrls: [ './collegues.component.scss' ]
 })
+
 export class ColleguesComponent implements OnInit {
 
   uneImage = "assets/logo.jpg";
-  unChamp = 'je uis un champ ts';
+  unChamp = 'je suis un champ ts';
 
   constructor() { }
 
