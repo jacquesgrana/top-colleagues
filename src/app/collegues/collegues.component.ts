a3913b64f2865555d66140fa31ca23736417f92b
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-08-24 17:24:42 +0200

    template

--- src/app/collegues/collegues.component.ts
+++ src/app/collegues/collegues.component.ts
@@ -2,10 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tc-collegues',
-  templateUrl: './collegues.component.html',
-  styleUrls: ['./collegues.component.scss']
+  template: `
+  <h1>Ici un template</h1>
+  <!-- [] => binding de propriete -->
+  <img [src]="uneImage" alt="une image par binding">
+
+  <!-- directive de structure ANGULAR -->
+  <p *nbIf="unChamp">
+    <!-- {{}} : interpolation : affiche d'une variable ou
+  d'un retour de méthode -->
+
+    {{unChamp}}
+    <!-- pipe : transforme l'affichage de la données -->
+
+    {{unChamp | uppercase}}
+  </p>
+  `
 })
 export class ColleguesComponent implements OnInit {
+
+  uneImage = "assets/logo.jpg";
+  unChamp = 'je uis un champ ts';
 
   constructor() { }
 
